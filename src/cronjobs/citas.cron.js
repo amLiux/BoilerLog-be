bf7a00a03241887434348d63622e4b7511276016
Icat <jardinbinario@protonmail.com>
2021-05-11 16:28:19 -0600

    Checking cronjob in Heroku

--- src/cronjobs/citas.cron.js
+++ src/cronjobs/citas.cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron')
 const Citas = require('../models/CitasModel')
 
 
-cron.schedule('59 23 * * *', async () => 
+cron.schedule('30 16 * * *', async () => 
     await Citas.updateMany({
         fechaDeseada: {$lt: new Date().setHours(23, 59, 59, 0)},
         estado: {$nin: ['CANCELADA', 'COMPLETADA']} 
