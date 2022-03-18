7981f4b7e76d3dd03bac8f7222e3cb471d7abad7
Marcelo J <marceliux@tutamail.com>
2022-03-17 21:51:20 -0600

    added new client

--- src/client/src/components/screens/CitasScreen.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CitasScreen = () => {
-    return (
-            <h1>Soy las citas</h1>
-    )
-}
--- src/client/src/components/ui/pacientes/PacientesSidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const PacientesSidebar = () => {
-    return (
-        <div className="sidenav">
-            
-        </div>
-    )
-}
